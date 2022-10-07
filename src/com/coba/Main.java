9efcbcfd367a164e208326b59f64869d24ddc863
Mesut Orhan <mesutor@gmail.com>
2022-10-07 00:07:32 -0400

    Palindrome class

--- src/com/coba/Main.java
+++ src/com/coba/Main.java
@@ -3,6 +3,7 @@ package com.coba;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Java Questions 1");
 	// write your code here
     }
 }
---
+++ src/com/coba/Palindrome.java
@@ -0,0 +1,4 @@
+package com.coba;
+
+public class Palindrome {
+}
