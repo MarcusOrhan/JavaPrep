18b50c5e4f35a6d7f3dad3b08bc9628b33a4e4f3
Mesut Orhan <mesutor@gmail.com>
2022-11-22 17:08:59 -0500

    Fibonacci simple solution added

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,21 +1,14 @@
 public class Fibonacci {
 
         public static void main(String[] args) {
-
             int a=0;
             int b=1;
-
             System.out.println(a + "  "+b +" ");
-
             for (int i=2; i<=10; i++){
                 //to eliminate 0+1 starting from 2
-
                 int c= a+b;
-
                 a=b;
-
                 b=c;
-
                 System.out.println("c = " + c);
             }
         }
