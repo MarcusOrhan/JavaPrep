66fb2ec35d775dcc6208d234b6248b23777c9119
Mesut Orhan <mesutor@gmail.com>
2022-11-22 17:24:53 -0500

    fibonacci with user entrance

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 public class Fibonacci {
 
         public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter a number to print fibonacci numbers");
+            int number = scan.nextInt();
             int a=0;
             int b=1;
             System.out.println(a + "  "+b +" ");
-            for (int i=2; i<=10; i++){
+            for (int i=2; i<=number; i++){
                 //to eliminate 0+1 starting from 2
                 int c= a+b;
                 a=b;
                 b=c;
-                System.out.println("c = " + c);
+                System.out.println("fibonacci #" +(i-1)+ " :"+ c);
+                scan.close();
             }
         }
 }
